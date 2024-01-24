a6b2dffe5d013ec2b16a9ee2c239a0e5763e2547
Tobias Blanck <tobias@sales2b.io>
2024-01-24 14:37:21 +0100

    add minimal sveltekit frontend

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Card } from 'flowbite-svelte';
+</script>
+
+<Card href="/queries">
+	<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> Ask Questions </h5>
+	<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight"> "We will answer everything</p>
+</Card>
