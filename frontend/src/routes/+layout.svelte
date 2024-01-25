12fd4efaa447bcf435c3bad026b3ba2b7d50ad86
Tobias Blanck <tobias@sales2b.io>
2024-01-25 10:15:37 +0100

    improve navbar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,17 +1,44 @@
 <script>
-	import '../app.pcss';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
+    import '../app.pcss';
+    import {
+        Avatar,
+        Dropdown,
+        DropdownDivider,
+        DropdownHeader,
+        DropdownItem,
+        Navbar,
+        NavBrand,
+        NavHamburger,
+        NavLi,
+        NavUl
+    } from 'flowbite-svelte';
 </script>
 
 
 <Navbar>
-	<NavBrand href="/">
-		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Edugrade</span>
-	</NavBrand>
-	<div class="flex md:order-2">
-		<Button size="sm">Get started</Button>
-		<NavHamburger />
-	</div>
+    <NavBrand href="/">
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Raggy</span>
+    </NavBrand>
+    <div class="flex items-center md:order-2">
+        <Avatar id="avatar-menu" />
+        <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+    </div>
+    <Dropdown placement="bottom" triggeredBy="#avatar-menu">
+        <DropdownHeader>
+            <span class="block text-sm">Jane Doe</span>
+            <span class="block truncate text-sm font-medium">jane@raggy.com</span>
+        </DropdownHeader>
+        <DropdownItem>Dashboard</DropdownItem>
+        <DropdownItem>Settings</DropdownItem>
+        <DropdownDivider />
+        <DropdownItem>Sign out</DropdownItem>
+    </Dropdown>
+    <NavUl class="md:order-1">
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/questions">Questions</NavLi>
+    </NavUl>
+
 </Navbar>
 
-<slot />
+
+<slot/>
