e589f92a6ca89c5fa1336c9b8e65a2e4b46ad428
Tobias Blanck <tobias@sales2b.io>
2024-01-25 17:05:08 +0100

    build milvus vector store

--- backend/retrieval/tools/index.py
+++ backend/retrieval/tools/index.py
@@ -3,8 +3,7 @@ from typing import List
 from llama_index import Document, VectorStoreIndex, StorageContext, ServiceContext
 from llama_index.embeddings import LangchainEmbedding
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from llama_index.vector_stores import ChromaVectorStore
-import chromadb
+from llama_index.vector_stores import MilvusVectorStore
 
 
 class DocumentIndex:
@@ -15,14 +14,19 @@ class DocumentIndex:
         self.service_context = self._build_service_context()
 
     def _build_storage_context(self):
-        client = chromadb.PersistentClient()
-        collection = client.get_or_create_collection(self.collection_name)
-        vector_store = ChromaVectorStore(chroma_collection=collection)
-        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        milvus_store = MilvusVectorStore(
+            uri="http://localhost:19530",
+            collection_name=self.collection_name,
+        )
+        storage_context = StorageContext.from_defaults(vector_store=milvus_store)
         return storage_context
 
     def _build_service_context(self):
-        return ServiceContext.from_defaults(embed_model=self.embedding_model, chunk_size=128, chunk_overlap=15)
+        return ServiceContext.from_defaults(
+            embed_model=self.embedding_model,
+            chunk_size=128,
+            chunk_overlap=15
+        )
 
     def embed_texts(self, texts: List[str]):
         for text in texts:
