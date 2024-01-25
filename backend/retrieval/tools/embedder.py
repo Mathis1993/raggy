e3ab840ade9768620bf306b63b3901a7e77e78ef
Tobias Blanck <tobias@sales2b.io>
2024-01-25 16:09:44 +0100

    add a basic document embedder which makes use of a vector database and can add additional documents

--- backend/retrieval/tools/embedder.py
+++ backend/retrieval/tools/embedder.py
@@ -1,10 +1,37 @@
+from typing import List
+
+from llama_index import Document, VectorStoreIndex, StorageContext, ServiceContext
 from llama_index.embeddings import LangchainEmbedding
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from llama_index.vector_stores import ChromaVectorStore
+import chromadb
 
 
 class DocumentEmbedder:
-    def __init__(self, model_path: str = "sentence-transformers/all-MiniLM-L6-v2"):
-        self.embedding_model = LangchainEmbedding(HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2'))
+    def __init__(self, model_path: str = "sentence-transformers/all-MiniLM-L6-v2", collection_name: str = "test"):
+        self.collection_name = collection_name
+        self.embedding_model = LangchainEmbedding(HuggingFaceEmbeddings(model_name=model_path))
+        self.storage_context = self._build_storage_context()
+        self.service_context = self._build_service_context()
 
-    def embed(self, document: str):
-        return self.model.encode(document)
+    def _build_storage_context(self):
+        client = chromadb.PersistentClient()
+        collection = client.get_or_create_collection(self.collection_name)
+        vector_store = ChromaVectorStore(chroma_collection=collection)
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        return storage_context
+
+    def _build_service_context(self):
+        return ServiceContext.from_defaults(embed_model=self.embedding_model)
+
+    def embed(self, texts: List[str]):
+        if not isinstance(texts, list):
+            raise ValueError("Texts must be a list of strings")
+
+        docs = [Document(text=text) for text in texts]
+        index = VectorStoreIndex(
+            storage_context=self.storage_context,
+            service_context=self.service_context
+        )
+        for doc in docs:
+            index.insert(doc)
