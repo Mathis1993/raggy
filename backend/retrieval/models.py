ea6e1a46bb09b657494e651b91303f751c849b7c
Tobias Blanck <tobias@sales2b.io>
2024-01-25 16:59:37 +0100

    add model for document

--- backend/retrieval/models.py
+++ backend/retrieval/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
+from retrieval.tools.index import DocumentIndex
 
-class ContextSource(models.Model):
+
+class Document(models.Model):
     name = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        indexer = DocumentIndex()
+        indexer.embed_text(self.text)
