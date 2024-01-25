c871149c701fa24273aef86913a62e2b31944f96
Tobias Blanck <tobias@sales2b.io>
2024-01-25 16:59:37 +0100

    add model for document

--- backend/questions/models.py
+++ backend/questions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Question(models.Model):
     question = models.CharField(max_length=255)
-    answer = models.TextField()
+    answer = models.TextField(null=True, blank=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
