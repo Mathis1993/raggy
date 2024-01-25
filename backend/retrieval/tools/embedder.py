7796e13aa3c1891ea4a6e5113faf89cd79143bee
Tobias Blanck <tobias@sales2b.io>
2024-01-25 15:47:43 +0100

    move requirements in backend folder

--- backend/retrieval/tools/embedder.py
+++ backend/retrieval/tools/embedder.py
@@ -1,9 +1,10 @@
-
+from llama_index.embeddings import LangchainEmbedding
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 
 
 class DocumentEmbedder:
     def __init__(self, model_path: str = "sentence-transformers/all-MiniLM-L6-v2"):
         self.embedding_model = LangchainEmbedding(HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2'))
 
-    def embed(self, document):
+    def embed(self, document: str):
         return self.model.encode(document)
