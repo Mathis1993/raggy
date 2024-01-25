bb4dcdaba692638e0c5ad553535b4a170e3360dd
Tobias Blanck <tobias@sales2b.io>
2024-01-25 16:59:50 +0100

    add serializer for documents

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
 from questions.models import Question
+from retrieval.models import Document
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
+        fields = "__all__"
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
         fields = "__all__"
