862724a17e4595d6499f6b61d5d8de0cdd55fdfe
Tobias Blanck <tobias@sales2b.io>
2024-01-26 17:19:28 +0100

    simplify page loader for questions

--- frontend/src/routes/questions/+page.ts
+++ frontend/src/routes/questions/+page.ts
@@ -1,10 +1,5 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }: { params: any }) {
-
-    console.log("Making request to http://localhost:8000/api/questions");
-    const response = await fetch('http://localhost:8000/api/questions');
-    console.log("Got response from http://localhost:8000/api/questions");
-    const questions = await response.json();
-    console.log(questions)
-    return questions
+    const response = await fetch('http://localhost:8000/api/questions/');
+    return await response.json();
 }
