3aab7d3956b282c89bfbba1b929d35f6fbdfda6f
Tobias Blanck <tobias.blanck96@gmail.com>
2024-01-29 23:28:01 +0100

    add Conversation and Message as models

--- backend/generation/models.py
+++ backend/generation/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Conversation(models.Model):
+    # Placeholder for now
+    user_id = models.IntegerField(default=1)
+
+    name = models.CharField(max_length=255, default=None, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_message_history(self):
+        return self.messages.order_by("created_at")[:10]
+
+
+class Message(models.Model):
+    conversation = models.ForeignKey(
+        Conversation, on_delete=models.CASCADE, related_name="messages"
+    )
+
+    text = models.TextField(default=None, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_user_message = models.BooleanField(default=False)
+    processing_time = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.text
