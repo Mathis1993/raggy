5546fea1761de25cf97df77e8788673187047593
Tobias Blanck <tobias@syntinels.com>
2024-01-30 14:39:17 +0100

    add serializers for chat and conversation

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from conversations.models import Conversation, Message
 from questions.models import Question
 from retrieval.models import Document
 
@@ -14,3 +15,17 @@ class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = ["id", "name", "url", "created_at"]
+
+
+class MessageSerializer(serializers.Serializer):
+    class Meta:
+        model = Message
+        fields = "__all__"
+
+
+class ConversationSerializer(serializers.Serializer):
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = "__all__"
