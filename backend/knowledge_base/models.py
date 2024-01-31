199c00a46a1e6c580ef78917b8aac9760e43b4ba
Mathis1993 <m_eric01@uni-muenster.de>
2024-01-31 16:03:11 +0100

    + Update model fields

--- backend/knowledge_base/models.py
+++ backend/knowledge_base/models.py
@@ -23,7 +23,7 @@ class Document(TrackCreation):
         PLAIN_TEXT = "plain_text"
 
     user_id = models.IntegerField()
-    identifier = models.CharField(max_length=2048)
+    identifier = models.CharField(max_length=2048, unique=True)
     doc_id = models.CharField(max_length=2048, null=True)
     type = models.CharField(choices=Type.choices, max_length=255)
     title = models.CharField(max_length=1024, null=True)
@@ -37,9 +37,12 @@ class Document(TrackCreation):
         self.save()
 
     def _ingest(self, url: str):
-        milvus_store = initialize_milvus_store(uri=f"http://{settings.MILVUS_HOST}:{settings.MILVUS_PORT}")
+        milvus_store = initialize_milvus_store(uri=f"http://{settings.MILVUS_HOST}:{settings.MILVUS_PORT}", load_collection=False)
 
-        document = BeautifulSoupWebReader().load_data([url])[0]
+        documents = BeautifulSoupWebReader().load_data([url])
+        if len(documents) == 0:
+            raise ValueError("No documents found.")
+        document = documents[0]
         document.metadata["user_id"] = self.user_id
         document.metadata["postgres_doc_id"] = self.pk
 
