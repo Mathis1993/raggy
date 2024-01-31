a88cfeb16274808303ae9035e545835be709a2d8
Mathis1993 <m_eric01@uni-muenster.de>
2024-01-31 16:03:11 +0100

    + Update model fields

--- backend/knowledge_base/tests/factories.py
+++ backend/knowledge_base/tests/factories.py
@@ -6,6 +6,10 @@ from knowledge_base.models import Document
 class DocumentFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Document
+        django_get_or_create = ("identifier",)
 
     user_id = factory.Faker("pyint")
+    identifier = factory.Sequence(lambda n: f"identifier_{n}")
+    type = Document.Type.WEBSITE
+    title = factory.Faker("sentence")
     content = factory.Faker("text")
