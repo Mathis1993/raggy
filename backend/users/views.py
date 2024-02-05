0dca3c0d2f3b7497cda74e65fa01132ceece27a3
Mathis1993 <m_eric01@uni-muenster.de>
2024-02-06 00:23:07 +0100

    #9 Add login view and url

---
+++ backend/users/views.py
@@ -0,0 +1,25 @@
+import http
+import json
+
+from django.contrib.auth import views as auth_views, authenticate, login
+from django.http import JsonResponse
+from django.middleware.csrf import get_token
+from django.views import View
+
+
+class CSRFTokenView(View):
+    def get(self, request, *args, **kwargs):
+        get_token(request)  # Sets a response cookie
+        return JsonResponse({"message": "CSRF cookie set"})
+
+
+class LoginView(auth_views.LoginView):
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"message": "Login successful"})
+        else:
+            return JsonResponse({"message": "Invalid credentials"}, status=http.HTTPStatus.BAD_REQUEST)
