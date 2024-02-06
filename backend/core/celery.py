a0543ae11562a0d9b0876156ce3c9da21c478bff
Tobias Blanck <tobias@syntinels.com>
2024-02-06 21:40:51 +0100

    #11 setup of redis, celery and django-redis

---
+++ backend/core/celery.py
@@ -0,0 +1,29 @@
+import os
+import ssl
+
+from celery import Celery
+from django.conf import settings
+
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+broker_use_ssl = None
+redis_backend_use_ssl = None
+if os.getenv("ENVIRONMENT", "development") == "production":
+    broker_use_ssl = {"ssl_cert_reqs": ssl.CERT_NONE}
+    redis_backend_use_ssl = {"ssl_cert_reqs": ssl.CERT_NONE}
+
+app = Celery(
+    "raggy",
+    broker_use_ssl=broker_use_ssl,
+    redis_backend_use_ssl=redis_backend_use_ssl,
+)
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
