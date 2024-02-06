ef979b17e679a622972c43ccf6b6fb3064ec3aac
Tobias Blanck <tobias@syntinels.com>
2024-02-06 12:48:47 +0100

    #10 update document UI using modals for create and delete and move API logic into a documentService

--- frontend/src/routes/documents/+page.svelte
+++ frontend/src/routes/documents/+page.svelte
@@ -3,7 +3,7 @@
         Breadcrumb,
         BreadcrumbItem,
         Button,
-        Card,
+        Card, Input, Label, Modal, Spinner,
         Table,
         TableBody,
         TableBodyCell,
@@ -12,19 +12,38 @@
         TableHeadCell
     } from "flowbite-svelte";
     import {Document} from "postcss";
+    import {goto, invalidate} from "$app/navigation";
+    import {createDocument, deleteDocument, getDocuments} from "./documentService";
+    import {ExclamationCircleOutline} from "flowbite-svelte-icons";
 
     export let data;
     let documents: [ContextDocument] = data.documents;
 
-    async function deleteDocument(documentId: number) {
-        const response = await fetch('http://localhost:8000/api/documents/' + documentId + '/', {
-            method: 'DELETE',
-        });
-        if (!response.ok) {
-            console.error('Failed to delete document', response.status);
-        }
-        location.reload();
+    let createModalVisible: boolean = false;
+    let deleteModalVisible: boolean = false;
+
+    let document_url: string = '';
+    let createProcessIsRunning: boolean = false;
+
+    async function handleCreate(document_url: string) {
+        createProcessIsRunning = true;
+        await createDocument(document_url);
+        createModalVisible = false;
+        createProcessIsRunning = false;
+        // TODO: fix invalidation and reloading of the data
+        await invalidate('/documents');
     }
+
+    let documentToDelete: any = null;
+    async function handleDelete() {
+        if (documentToDelete !== null) {
+            await deleteDocument(documentToDelete);
+            deleteModalVisible = false;
+            // TODO: fix invalidation and reloading of the data
+            await invalidate('/documents');
+        }
+    }
+
 </script>
 
 <Card class="max-w-full w-full">
@@ -32,14 +51,13 @@
     <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight"> This is a list of your uploaded documents </p>
 
     <div class="flex justify-end my-2">
-        <Button href="/documents/create">Add Document</Button>
+        <Button on:click={() => (createModalVisible = true)}>Add Document</Button>
     </div>
 
     <Table hoverable={true}>
         <TableHead>
             <TableHeadCell>Document Name</TableHeadCell>
             <TableHeadCell>URL</TableHeadCell>
-            <TableHeadCell>Created At</TableHeadCell>
             <TableHeadCell>
                 <span class="sr-only">Details</span>
             </TableHeadCell>
@@ -47,16 +65,44 @@
         <TableBody class="divide-y">
             {#each documents as document}
                 <TableBodyRow>
-                    <TableBodyCell>{document.title}</TableBodyCell>
+                    <TableBodyCell>{document.title ? document.title.substring(0, 50) : 'No title'}</TableBodyCell>
                     <TableBodyCell>{document.identifier}</TableBodyCell>
-                    <TableBodyCell>{document.created_at}</TableBodyCell>
                     <TableBodyCell>
                         <a href="/documents/{document.id}/" class="font-medium text-primary-600 hover:underline dark:text-primary-500">View</a>
                     </TableBodyCell>
                     <TableBodyCell>
-                        <Button on:click={() => deleteDocument(document.id)} variant="danger">Delete</Button>                    </TableBodyCell>
+                        <Button on:click={() => {deleteModalVisible = true; documentToDelete = document.id;}}>Delete</Button>
+                    </TableBodyCell>
                 </TableBodyRow>
             {/each}
         </TableBody>
     </Table>
 </Card>
+
+
+<Modal bind:open={createModalVisible} size="xs" autoclose={false} outsideclose={true} class="w-full">
+    <form class="flex flex-col space-y-6" on:submit|preventDefault={() => handleCreate(document_url)}>
+        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Add a new document</h3>
+        <Label class="space-y-2">
+            <span>URL</span>
+            <Input bind:value={document_url} type="url" name="url" placeholder="www.company.com" required />
+        </Label>
+        <Button type="submit" class="w-full1" disabled={createProcessIsRunning}>
+            {#if createProcessIsRunning}
+                <Spinner/>
+            {:else}
+                Add
+            {/if}
+        </Button>
+    </form>
+</Modal>
+
+
+<Modal bind:open={deleteModalVisible} autoclose={false} outsideclose={true} size="xs">
+    <div class="text-center">
+        <ExclamationCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
+        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to remove this document?</h3>
+        <Button on:click={handleDelete} color="red" class="me-2">Yes, I'm sure</Button>
+        <Button color="alternative">No, cancel</Button>
+    </div>
+</Modal>
--- frontend/src/routes/documents/+page.ts
+++ frontend/src/routes/documents/+page.ts
@@ -1,5 +1,6 @@
+import {getDocuments} from "./documentService";
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }: { params: any }) {
-    const response = await fetch('http://localhost:8000/api/documents/');
-    return await response.json();
+    return await getDocuments();
 }
