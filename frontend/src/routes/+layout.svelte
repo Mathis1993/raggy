04b439dde2635e9d0660b9aab5536c172dc562c2
Tobias Blanck <tobias@syntinels.com>
2024-02-06 19:10:36 +0100

    #10 improve UI of sidebar; load data async and auto render sidebar if not told otherwise

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,115 +3,69 @@
     import {
         Avatar,
         Button,
-        Drawer,
         Dropdown,
         DropdownDivider,
         DropdownHeader,
         DropdownItem,
         Navbar,
         NavBrand,
-        NavHamburger,
         Sidebar,
         SidebarGroup,
         SidebarItem,
         SidebarWrapper,
+        Spinner,
     } from 'flowbite-svelte';
-    import {BookSolid} from "flowbite-svelte-icons";
-    import {onMount} from "svelte";
+    import {BarsSolid, FileSearchSolid, OpenBookSolid, PlusSolid} from "flowbite-svelte-icons";
     import {page} from "$app/stores";
-    import {goto, invalidateAll} from "$app/navigation";
-
+    import {onMount} from "svelte";
+    import {createConversation} from "./conversations/conversationService";
 
     let breakPoint: number = 1024;
-    let width: number = 72;
-    let activateClickOutside = true;
-    let drawerHidden: boolean = false;
-
-    $: if (width >= breakPoint) {
-        drawerHidden = false;
-        activateClickOutside = false;
-    } else {
-        drawerHidden = true;
-        activateClickOutside = true;
-    }
-
-    onMount(() => {
-        if (width >= breakPoint) {
-            drawerHidden = false;
-            activateClickOutside = false;
-        } else {
-            drawerHidden = true;
-            activateClickOutside = true;
-        }
-    });
-    const toggleSide = () => {
-        if (width < breakPoint) {
-            drawerHidden = !drawerHidden;
-        }
-    };
-    const toggleDrawer = () => {
-        drawerHidden = !drawerHidden;
-    };
-
+    let width: number = typeof window !== 'undefined' ? window.innerWidth : 1024;
+    let sidebarVisible: boolean = true;
+    $: sidebarVisible = width >= breakPoint;
     $: activeUrl = $page.url.pathname;
 
+    console.log($page.url.pathname);
+
     export let data;
-    let conversations: Conversation[] = data.conversations;
+    $: conversations = $page.data.conversations || [];
+    $: loadingConversations = !conversations.length;
 
-    let transitionParams = {
-        duration: 0,
-        transitionType: 'fly',
-    };
+    onMount(async () => {
+        width = window.innerWidth;
+        sidebarVisible = width >= breakPoint;
+    });
 
-    let createConversation = async () => {
-        console.log('Creating conversation');
-        try {
-            const response = await fetch('http://localhost:8000/api/conversations/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
-
-            if (!response.ok) {
-                console.error('Failed to create conversation');
-                return;
-            }
-
-            console.log('Conversation created');
-
-            const data = await response.json();
-            await goto(`/conversations/${data.id}`, {replaceState: true});
-            await invalidateAll();
-        } catch (error) {
-            console.error('Failed to create conversation', error);
+    function toggleSidebar() {
+        if (width < breakPoint) {
+            sidebarVisible = !sidebarVisible;
         }
     }
 
 </script>
 
-
 <svelte:window bind:innerWidth={width}/>
 
 
 <header class="flex-none w-full mx-auto bg-white dark:bg-slate-950">
-
-    <Navbar class="fixed flex items-center h-[calc(100vh-93vh)] bg-gray-700 border-b border-gray-400">
-        <Button
-                on:click={toggleDrawer}
-                class="fixed left-4 bg-gray-700 focus:outline-none whitespace-normal rounded-lg focus:ring-2 p-1.5 focus:ring-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600 m-0 mr-3"
-        >
-            <NavHamburger class="w-6 h-6"/>
-        </Button>
-        <NavBrand href="/" class="text-gray-50">
-            <span class="self-center whitespace-nowrap text-xl font-semibold">Raggy</span>
-            <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                 fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M16.9 9.7 20 6.6 17.4 4 4 17.4 6.6 20 16.9 9.7Zm0 0L14.3 7M6 7v2m0 0v2m0-2H4m2 0h2m7 7v2m0 0v2m0-2h-2m2 0h2M8 4h0v0h0v0Zm2 2h0v0h0v0Zm2-2h0v0h0v0Zm8 8h0v0h0v0Zm-2 2h0v0h0v0Zm2 2h0v0h0v0Z"/>
-            </svg>
-        </NavBrand>
-        <div class="flex items-center md:order-2">
+    <Navbar class="fixed flex items-center min-h-10 justify-between h-[calc(100vh-93vh)] bg-gray-700 border-b border-gray-400">
+        <div class="absolute left-5 flex">
+            {#if width < breakPoint}
+                <Button on:click={toggleSidebar} class="bg-gray-700 hover:bg-gray-500 mr-2 px-2 py-1">
+                    <BarsSolid class="w-4 h-4"/>
+                </Button>
+            {/if}
+            <NavBrand href="/" class="text-gray-50">
+                <span class="self-center whitespace-nowrap text-xl font-semibold">Raggy</span>
+                <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                     fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M16.9 9.7 20 6.6 17.4 4 4 17.4 6.6 20 16.9 9.7Zm0 0L14.3 7M6 7v2m0 0v2m0-2H4m2 0h2m7 7v2m0 0v2m0-2h-2m2 0h2M8 4h0v0h0v0Zm2 2h0v0h0v0Zm2-2h0v0h0v0Zm8 8h0v0h0v0Zm-2 2h0v0h0v0Zm2 2h0v0h0v0Z"/>
+                </svg>
+            </NavBrand>
+        </div>
+        <div class="absolute right-5 flex items-center md:order-2">
             <Avatar id="avatar-menu"/>
         </div>
         <Dropdown placement="bottom" triggeredBy="#avatar-menu">
@@ -127,44 +81,67 @@
     </Navbar>
 </header>
 
-<div id="sidebar" class="overflow-y-auto z-50 p-4 dark:bg-gray-800 w-72 fixed start-0 inset-y-[calc(100vh-93vh)] h-full bg-gray-700">
-    <Sidebar class="w-full">
-        <SidebarWrapper class="bg-gray-700 p-0">
-            <SidebarGroup class="text-white">
-                <SidebarItem class="text-white hover:bg-gray-500"
-                             data-sveltekit-reload label="Start New" on:click={createConversation}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                         class="h-6 w-6">
-                        <path d="M12 6v6m0 0v6m0-6h6m-6 0H6" stroke-linecap="round" stroke-linejoin="round"
-                              stroke-width="2"></path>
-                    </svg>
-                </SidebarItem>
+{#if sidebarVisible}
+    <div id="sidebar"
+         class="overflow-y-auto z-50 px-2 pt-4 dark:bg-gray-800 w-72 fixed start-0 inset-y-[calc(100vh-93vh)] h-full bg-gray-700">
+        <Sidebar class="w-full">
+            <SidebarWrapper class="bg-gray-700 p-0">
+                <SidebarGroup class="text-white">
+                    <div class="flex items-center justify-between">
+                        <h2 class="font-semibold text-lg ml-4">
+                            Conversations
+                        </h2>
+                        <Button size="sm" class="ml-4 bg-gray-700 hover:bg-gray-500" on:click={createConversation}>
+                            <PlusSolid class="w-4 h-4"/>
+                        </Button>
+                    </div>
+                    <div class="max-h-[calc(100vh-40vh)] overflow-y-auto">
+                        {#if loadingConversations}
+                            <div>
+                                <Spinner/>
+                            </div>
+                        {:else}
+                            {#each conversations as conversation}
+                                <SidebarItem class="text-white hover:bg-gray-500"
+                                             label={conversation.name || "Start Conversation..." }
+                                             data-sveltekit-reload
+                                             href={`/conversations/${conversation.id}`}
+                                             on:click={toggleSidebar}
+                                             active={activeUrl === `conversations/`}>
+                                </SidebarItem>
+                                <!--                                    <Button on:click={() => {deleteConversation(conversation.id); toggleSidebar()}}-->
+                                <!--                                            class="ml-2 bg-gray-700 hover:bg-gray-500">-->
+                                <!--                                        <TrashBinOutline class="w-4 h-4"/>-->
+                                <!--                                    </Button>-->
+                            {/each}
+                        {/if}
+                    </div>
+                </SidebarGroup>
 
-                {#each conversations as conversation}
-                    <SidebarItem class="text-white hover:bg-gray-500"
-                                 label={conversation.name || "Start Conversation..." } data-sveltekit-reload
-                                 href={`/conversations/${conversation.id}`} on:click={toggleSide}
-                                 active={activeUrl === `conversations/`}>
+                <SidebarGroup border class="w-full inset-y-[calc(100vh-30vh)] mt-6">
+                    <SidebarItem class="text-white hover:bg-gray-500" label="Documents" href="/documents"
+                                 on:click={toggleSidebar} active={activeUrl === "/documents"}>
+                        <svelte:fragment slot="icon">
+                            <FileSearchSolid
+                                    class="w-5 h-5 text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
+                        </svelte:fragment>
                     </SidebarItem>
-                {/each}
-
-            </SidebarGroup>
-
-            <SidebarGroup border>
-                <SidebarItem class="text-white hover:bg-gray-500" label="Documents" href="/documents">
-                    <svelte:fragment slot="icon">
-                        <BookSolid
-                                class="w-5 h-5 text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
-                    </svelte:fragment>
-                </SidebarItem>
-            </SidebarGroup>
-        </SidebarWrapper>
-    </Sidebar>
-</div>
-
+                    <SidebarItem class="text-white hover:bg-gray-500" label="Information"
+                                 href="https://www.llamaindex.ai/" target="_blank" on:click={toggleSidebar}>
+                                 on:click={toggleSidebar}>
+                        <svelte:fragment slot="icon">
+                            <OpenBookSolid
+                                    class="w-5 h-5 text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
+                        </svelte:fragment>
+                    </SidebarItem>
+                </SidebarGroup>
+            </SidebarWrapper>
+        </Sidebar>
+    </div>
+{/if}
 
 <div class="flex px-4 mx-auto w-full pt-[calc(100vh-90vh)] h-[calc(100vh-10vh)]">
     <main class="lg:ml-72 w-full mx-auto">
-        <slot />
+        <slot/>
     </main>
 </div>
