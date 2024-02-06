8cbf2269d3e1e64a46d8c7c1d0e202af85ecd011
Tobias Blanck <tobias@syntinels.com>
2024-02-06 21:31:02 +0100

    #11 add status to document model

--- backend/knowledge_base/models.py
+++ backend/knowledge_base/models.py
@@ -10,6 +10,7 @@ from llama_index.readers import BeautifulSoupWebReader
 from llama_index.text_splitter import SentenceSplitter
 
 from core.models import TrackCreation
+from core.utils.models import model_save
 from knowledge_base.vector_store import initialize_milvus_store
 
 logger = logging.getLogger(__name__)
@@ -25,12 +26,18 @@ class Document(TrackCreation):
         PDF = "pdf"
         PLAIN_TEXT = "plain_text"
 
+    class Status(models.TextChoices):
+        PROCESSING = "processing"
+        COMPLETED = "completed"
+        FAILED = "failed"
+
     user_id = models.IntegerField()
     identifier = models.CharField(max_length=2048)
     doc_id = models.CharField(max_length=2048, null=True)
     type = models.CharField(choices=Type.choices, max_length=255)
     title = models.CharField(max_length=1024, null=True)
     content = models.TextField(null=True)
+    status = models.CharField(choices=Status.choices, max_length=255, default=Status.PROCESSING)
 
     def __str__(self):
         return f"{self.identifier} ({self.type})"
@@ -40,22 +47,27 @@ class Document(TrackCreation):
             raise ValueError("Document must be saved before ingestion.")
 
         self._ingest(url)
+        self.mark_as_completed(save=False)
         self.save()
 
     def delete_and_digest(self):
         self._digest()
         self.delete()
 
+    @model_save(update_fields=["status"])
+    def mark_as_failed(self):
+        self.status = Document.Status.FAILED
+
+    @model_save(update_fields=["status"])
+    def mark_as_completed(self):
+        self.status = Document.Status.COMPLETED
+
     def _ingest(self, url: str):
         milvus_store = initialize_milvus_store(
             uri=f"http://{settings.MILVUS_HOST}:{settings.MILVUS_PORT}",
             load_collection=False
         )
-
-        documents = BeautifulSoupWebReader().load_data([url])
-        if len(documents) == 0:
-            raise ValueError(f"Content extraction from url {url} failed.")
-        document = documents[0]
+        document = self._extract_document_from_url(url)
         document.metadata["user_id"] = self.user_id
         document.metadata["postgres_doc_id"] = self.pk
 
@@ -70,14 +82,22 @@ class Document(TrackCreation):
             # TODO: Can we use the vector_store parameter of the pipeline here?
         )
         nodes = pipeline.run(documents=[document])
-        logger.info(f"Extracted {len(nodes)} nodes from document.")
         milvus_store.add([n for n in nodes if n.embedding is not None])
+        logger.info(f"Added {len(nodes)} nodes from document.")
 
         self.doc_id = document.doc_id
         self.content = document.text
         self.type = Document.Type.WEBSITE
         if len(nodes) > 0:
             self.title = nodes[0].metadata["document_title"]
+
+    @staticmethod
+    def _extract_document_from_url(url: str):
+        documents = BeautifulSoupWebReader().load_data([url])
+        if len(documents) == 0:
+            raise ValueError(f"Content extraction from url {url} failed.")
+        document = documents[0]
+        return document
 
     def _digest(self):
         milvus_store = initialize_milvus_store(
