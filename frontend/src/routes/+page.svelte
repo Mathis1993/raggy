68998c0004c70722f36bcbe45a94f8f1d6eafcae
Tobias Blanck <tobias@syntinels.com>
2024-02-06 19:10:36 +0100

    #10 improve UI of sidebar; load data async and auto render sidebar if not told otherwise

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,34 +1,9 @@
 <script lang="ts">
     import {Button} from 'flowbite-svelte';
-    import {goto, invalidateAll} from "$app/navigation";
-
-    let createConversation = async () => {
-        console.log('Creating conversation');
-        try {
-            const response = await fetch('http://localhost:8000/api/conversations/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-            });
-
-            if (!response.ok) {
-                console.error('Failed to create conversation');
-                return;
-            }
-
-            console.log('Conversation created');
-
-            const data = await response.json();
-            await goto(`/conversations/${data.id}`, {replaceState: true});
-            await invalidateAll();
-        } catch (error) {
-            console.error('Failed to create conversation', error);
-        }
-    }
+    import {createConversation} from "./conversations/conversationService";
 </script>
 
-<div class="flex items-center justify-center">
+<div class="flex h-full items-center justify-center">
     <Button data-sveltekit-reload on:click={createConversation} color="primary" size="lg">Create conversation</Button>
 </div>
 
