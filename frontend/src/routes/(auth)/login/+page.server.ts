85c44ad10ec8f2e14f0fbde24be0329d891e03f7
Mathis1993 <m_eric01@uni-muenster.de>
2024-02-07 14:14:57 +0100

    #9 Use httponly false cookie for csrf

--- frontend/src/routes/(auth)/login/+page.server.ts
+++ frontend/src/routes/(auth)/login/+page.server.ts
@@ -2,7 +2,9 @@ import { parse } from 'cookie';
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('../../../../.svelte-kit/types/src/routes').PageLoad} */
-// Get the csrf token and set it as a cookie
+// Get the csrf token and set it as a cookie on the client in preparation of the login POST request
+// (httpOnly is false as we need to extract the token from the cookie for later client-side post requests)
+// (but django docs say that is okay -> https://docs.djangoproject.com/en/5.0/ref/settings/#std-setting-CSRF_COOKIE_HTTPONLY)
 export async function load({ cookies }) {
 	const response = await fetch('http://127.0.0.1:8000/users/csrf/', {credentials: 'include'});
 	const setCookieHeader = response.headers.get('set-cookie');
@@ -12,7 +14,7 @@ export async function load({ cookies }) {
 		const csrfToken = parsedCookies['csrftoken'];
 
 		if (csrfToken) {
-			cookies.set('csrftoken', csrfToken,  { path: '/'});
+			cookies.set('csrftoken', csrfToken,  { path: '/', httpOnly: false});
 		}
 	}
 	return await response.json();
