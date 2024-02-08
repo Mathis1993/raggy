b5254a4f972b47ac4170f836d815ce19d83cf72d
Tobias Blanck <tobias@syntinels.com>
2024-02-08 17:11:23 +0100

    #15 fix parser config to allow PDF uploads

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -66,6 +66,16 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "config.urls"
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.JSONParser'
+    )
+}
+
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
