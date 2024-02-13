250b02e03d28fe546689c7bb3addd13426b617c7
Tobias Blanck <tobias@syntinels.com>
2024-02-13 22:23:21 +0100

    #22 fix load functions now receiving paginated responses

--- frontend/src/routes/(app)/+layout.ts
+++ frontend/src/routes/(app)/+layout.ts
@@ -6,5 +6,5 @@ export async function load({ fetch }) {
     const userResponse = await fetch('http://127.0.0.1:8000/users/info/', {credentials: 'include'});
     const user: User = await userResponse.json()
     console.log("Loaded user info")
-    return {"conversations": conversations, "user": user, "conversationsLoaded": true}
+    return {"conversations": conversations.results, "user": user, "conversationsLoaded": true}
 }
