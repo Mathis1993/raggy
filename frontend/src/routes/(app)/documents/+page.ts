1d7f31a02c7b1fe1e9c5c560f746ad9e23d0c727
Tobias Blanck <tobias@syntinels.com>
2024-02-14 17:00:38 +0100

    #25 implement "load more" pagination

--- frontend/src/routes/(app)/documents/+page.ts
+++ frontend/src/routes/(app)/documents/+page.ts
@@ -1,4 +1,5 @@
 import {getDocuments} from "./documentService";
+import {page} from "$app/stores";
 
 // Only run client-side to ensure credentials are available for fetch function
 export const ssr = false;
@@ -8,5 +9,6 @@ export async function load({ params, url }) {
     const documentType = url.searchParams.get('type') || "";
     const search = url.searchParams.get('search') || "";
     const documents = await getDocuments(documentType, search);
-    return {documents: documents.results};
+    documents.currentPage = 1;
+    return documents;
 }
