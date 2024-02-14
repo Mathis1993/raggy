6b86998d7d3cf79541306ef9886a125c196e350e
Mathis1993 <m_eric01@uni-muenster.de>
2024-02-15 00:02:22 +0100

    #24 Add raggy icon to login page

--- frontend/src/routes/(auth)/login/+page.svelte
+++ frontend/src/routes/(auth)/login/+page.svelte
@@ -36,6 +36,14 @@
         </Banner>
     {/if}
     <Register href="/">
+        <svelte:fragment slot="top">
+            <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Raggy</span>
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                 fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M16.9 9.7 20 6.6 17.4 4 4 17.4 6.6 20 16.9 9.7Zm0 0L14.3 7M6 7v2m0 0v2m0-2H4m2 0h2m7 7v2m0 0v2m0-2h-2m2 0h2M8 4h0v0h0v0Zm2 2h0v0h0v0Zm2-2h0v0h0v0Zm8 8h0v0h0v0Zm-2 2h0v0h0v0Zm2 2h0v0h0v0Z"/>
+            </svg>
+        </svelte:fragment>
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <form class="flex flex-col space-y-6" on:submit|preventDefault={handleSubmit}>
                 <h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Sign In</h3>
@@ -51,7 +59,7 @@
                     <p class="text-red-500">{ $loginError }</p>
                 {/if}
                 <div class="flex items-start">
-                    <a href="/static" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot
+                    <a href="/reset-password" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot
                         password?</a>
                 </div>
                 <Button type="submit" class="w-full1">Sign in</Button>
