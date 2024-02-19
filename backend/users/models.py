3ce333e9666a7c3f981bcd3ec0e92a58ab2ffed0
Tobias Blanck <tobias@syntinels.com>
2024-02-19 16:20:24 +0100

    #33 delete user's document dir when user is deleted

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,7 @@
+import os
+import shutil
+
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import TextChoices
@@ -6,12 +10,18 @@ from django.utils.translation import gettext_lazy as _
 from core.models import TrackUpdates
 
 
+def delete_user_directory(instance):
+    directory = os.path.join(settings.MEDIA_ROOT, f'documents/user_{instance.id}')
+    if os.path.isdir(directory):
+        shutil.rmtree(directory)
+
+
 class User(AbstractUser):
     class Meta:
         db_table = "users_users"
 
     username = models.CharField(null=True, blank=True, default=None)  # We don't want to use this field
-    email = models.EmailField(_("email address"), unique=True) # Make email field unique
+    email = models.EmailField(_("email address"), unique=True)  # Make email field unique
     email_verified = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
@@ -25,6 +35,10 @@ class User(AbstractUser):
         super().save(*args, **kwargs)
         if is_new:
             UserSettings.objects.get_or_create(user=self)
+
+    def delete(self, using=None, keep_parents=False):
+        delete_user_directory(self)
+        super().delete(using, keep_parents)
 
 
 class UserSettings(TrackUpdates):
