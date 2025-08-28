6f68a4ee87f407f10aa7182ad17483fc2445f246
Mathis1993 <m_eric01@uni-muenster.de>
2025-08-28 20:27:37 +0200

    Add user manager

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,13 +1,13 @@
 import os
 import shutil
 
+from core.models import TrackUpdates
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import UserManager as DjangoUserManager
 from django.db import models
 from django.db.models import TextChoices
 from django.utils.translation import gettext_lazy as _
-
-from core.models import TrackUpdates
 
 
 def delete_user_directory(instance):
@@ -16,16 +16,46 @@ def delete_user_directory(instance):
         shutil.rmtree(directory)
 
 
+class UserManager(DjangoUserManager):
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
 class User(AbstractUser):
     class Meta:
         db_table = "users_users"
 
-    username = models.CharField(null=True, blank=True, default=None)  # We don't want to use this field
+    username = None  # We don't want to use this field
     email = models.EmailField(_("email address"), unique=True)  # Make email field unique
     email_verified = models.BooleanField(default=False)
 
+    EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
@@ -42,7 +72,6 @@ class User(AbstractUser):
 
 
 class UserSettings(TrackUpdates):
-
     class Meta:
         db_table = "users_user_settings"
 
